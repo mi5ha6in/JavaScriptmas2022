a8ebb7e703adfb7b441f943ffcfeacf06d57de6b
mi5ha6in <mi5ha6in@gmail.com>
2022-12-10 23:28:39 +0300

    adds day 10

---
+++ day10/index.js
@@ -0,0 +1,9 @@
+function sortByLength(strs) {
+  //  write code here.
+  return strs.sort((a, b) => a.length - b.length);
+}
+
+console.log(
+  sortByLength(["abc", "", "aaa", "a", "zz"]),
+  'expect: ["", "a", "zz", "abc", "aaa"]'
+);
